2d1af721b815258e558a928bb753c315a06a69a6
Vedant Bhoj <bhoj.vedant28@gmail.com>
2019-10-22 08:45:01 -0700

    Committing the lab assignment

---
+++ ChatApp/HW3/chat/index.js
@@ -0,0 +1,99 @@
+// Setup basic express server
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').createServer(app);
+var io = require('../..')(server);
+var port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log('Server listening at port %d', port);
+});
+
+// Routing
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Chatroom
+
+var numUsers = 0;
+var connectedUsers = {};
+
+io.on('connection', (socket) => {
+  var addedUser = false;
+
+
+  socket.on("private", function(data) {   
+    const to = data.to,
+    message = data.msg;
+    console.log("Private message received to: "+ to );
+if(connectedUsers.hasOwnProperty(to)){
+connectedUsers[to].emit('private',{
+    //The sender's username
+    username : socket.username,
+    //Message sent to receiver
+    message : message
+});
+}
+    //socket.emit("private", { from: socket.id, to: data.to, msg: data.msg });
+});
+
+  // when the client emits 'new message', this listens and executes
+  socket.on('new message', (data) => {
+    // we tell the client to execute 'new message'
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      message: data
+    });
+    console.log(socket.username + "broadcasted a message with content: "+ data );
+
+  });
+
+  // when the client emits 'add user', this listens and executes
+  socket.on('add user', (username) => {
+    if (addedUser) return;
+
+    // we store the username in the socket session for this client
+    socket.username = username;    
+    connectedUsers[username] = socket;
+    ++numUsers;
+    addedUser = true;
+    socket.emit('login', {
+      numUsers: numUsers
+    });
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+      numUsers: numUsers
+    });
+
+    console.log(socket.username+" has joined the chat room.");
+  });
+
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', () => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
+  });
+
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', () => {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  // when the user disconnects.. perform this
+  socket.on('disconnect', () => {
+    if (addedUser) {
+      --numUsers;
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username,
+        numUsers: numUsers
+      });
+      console.log(socket.username+" left the chat room.");
+    }
+  });
+});
---
+++ Readme.md
@@ -0,0 +1,16 @@
+***HW #3***
+----
+**Chat App**
+The TCP Server responds with a message on connecting with the client.
+Used Socket.IO library to implement this application.
+----
+**Team name: The Original**
+
+----
+Functionality:
+#Add support for nicknames.Don’t send the same message to the user that sent it himself.
+#Instead,append the message directly as soon as he presses enter.
+#Add “{user} is typing” functionality.
+#Show who’s online
+#Add private messaging. 
+
